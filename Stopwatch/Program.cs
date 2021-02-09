83a05f31fc758bb4188502f634fb4cc5d6aec7a8
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-09 13:16:33 -0300

    Cronometro - Opcoes do menu

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -13,10 +13,13 @@ namespace Stopwatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segunto");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segunto => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); //ToLower coloca para minusculo
+            Console.WriteLine(data);
         }
 
         static void Start(int time)
