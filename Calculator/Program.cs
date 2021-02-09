d9362bd2ee28faa7d3e4525f5d2ae764c2c548bb
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-09 11:17:29 -0300

    Criando uma calculadora - Saindo da aplicacao

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,6 +18,7 @@ namespace Calculator
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisao");
             Console.WriteLine("4 - Multiplicacao");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("------------");
             Console.WriteLine("Selecione uma opção: ");
@@ -30,6 +31,7 @@ namespace Calculator
                 case 2: Subtracao(); break;
                 case 3: Divisao(); break;
                 case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
                 default: Menu(); break;
             }
 
