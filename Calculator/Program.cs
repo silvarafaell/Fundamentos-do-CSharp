40af85fc02bdf2952f3e52a7a48d3d67ac15eaa2
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-09 08:46:19 -0300

    Criando uma calculadora - Soma

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,10 +7,17 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Primeiro Valor: ");
+
+            Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é " + resultado);
         }
     }
 }
