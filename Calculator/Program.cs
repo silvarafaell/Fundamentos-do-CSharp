485be6985d4a18c85f5cde86f8fdea9c59704f8d
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-09 08:30:54 -0300

    Criando uma calculadora - Iniciando o projeto

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
