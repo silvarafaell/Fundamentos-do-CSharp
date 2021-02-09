f0b87c020f100a598bd283f2afa4517ae4aebbb9
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-09 12:25:28 -0300

    Cronometro - Estrutura do cronometro

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -8,5 +8,16 @@ namespace Stopwatch
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+            }
+        }
     }
 }
