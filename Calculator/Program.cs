aff4233a7553dd5613ab5de7be5f0906ca1cb287
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-09 09:32:48 -0300

    Criando uma calculadora - Divisao

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Subtracao();
+            Divisao();
         }
 
         static void Soma()
@@ -40,6 +40,23 @@ namespace Calculator
 
             float resultado = v1 - v2;
             Console.WriteLine("O resultado da subtração é " + resultado);
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine("O resultado da divisão é " + resultado);
             Console.ReadKey();
         }
     }
