411d459f5d362788edfcdcd0a522eddc9975060f
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-09 20:48:57 -0300

    Cronometro - Finalizando a aplicacao

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -29,7 +29,20 @@ namespace Stopwatch
             if (time == 0)
                 System.Environment.Exit(0);
 
-            Start(time * multiplier);
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
         }
 
         static void Start(int time)
