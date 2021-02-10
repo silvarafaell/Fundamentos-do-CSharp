69835fd70b2c2ee925dfd44c6932a73d966b68c5
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-09 21:31:09 -0300

    Editor de Textos - Iniciando o projeto

---
+++ TextEditor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que voce deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+
+        }
+
+        static void Editar()
+        {
+
+        }
+    }
+}
