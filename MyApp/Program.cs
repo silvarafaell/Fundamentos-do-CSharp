f42f82f9852809b5ae229917beb655d0663605fa
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-11 10:45:34 -0300

    Strings - Guids

---
+++ MyApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid(); // gera com id novo
+            id.ToString();
+
+            id = new Guid("7874f71e-d53a-42d8-b7b1-4457f5b49bc3");
+
+            if (id == Guid.NewGuid())
+                Console.WriteLine(id.ToString().Substring(0, 8)); //SubString para pegar os 8 primeiros caracteres
+        }
+    }
+}
