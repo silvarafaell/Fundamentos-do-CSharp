44db4a7424d46edcb88f7f70ed434d9d27f82fd3
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-11 13:01:00 -0300

    Strings - StartsWith/EndsWith

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,10 +6,16 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            //começa com e termina com
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.Contains("teste"));
-            Console.WriteLine(texto.Contains("Teste"));
-            Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase)); // maiuscula ou minuscula
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(texto.StartsWith("texto"));
+
+            Console.WriteLine(texto.EndsWith("texte"));
+            Console.WriteLine(texto.EndsWith("Texte"));
+            Console.WriteLine(texto.EndsWith("xpro"));
+
         }
     }
 }
