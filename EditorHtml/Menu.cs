3b7e2dcc765b69fef4ac37ac7189522bc4072938
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-12 11:50:41 -0300

    Editor HTML - Menu da aplicacao

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -14,6 +14,7 @@ namespace EditorHtml
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -61,6 +62,23 @@ namespace EditorHtml
             Console.SetCursorPosition(3, 10);
             Console.WriteLine("Opção: ");
         }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
+        }
+
     }
 
 }
