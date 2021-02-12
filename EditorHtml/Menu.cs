a9a0ed86b36b1fcc889ee4054aaead11cffbf4a0
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-12 08:42:42 -0300

    Editor HTML - Iniciando o projeto

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue; //cor de fundo
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            
+        }
+    }
+}
