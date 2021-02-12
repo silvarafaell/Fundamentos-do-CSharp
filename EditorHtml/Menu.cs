f03562b3d85cd88add360f6d2197da48f4114e35
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-12 11:32:12 -0300

    Editor HTML - Exibindo o Menu

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -11,6 +11,9 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         public static void DrawScreen()
@@ -40,5 +43,24 @@ namespace EditorHtml
             Console.Write("\n");
 
         }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console..WriteLine("===========");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.WriteLine("Opção: ");
+        }
     }
+
 }
