e09949df79ac6c3b9af5dbc62e598897ff8d6319
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-12 09:18:56 -0300

    Editor HTML - Desenhando a tela

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -10,7 +10,35 @@ namespace EditorHtml
             Console.BackgroundColor = ConsoleColor.Blue; //cor de fundo
             Console.ForegroundColor = ConsoleColor.Black;
 
-            
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
         }
     }
 }
