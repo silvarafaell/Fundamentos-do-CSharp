35e344e9d33370f0433663a5bc66aedf16e5c6d0
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-12 15:09:48 -0300

    Editor HTML - Substituindo caracteres

--- EditorHtml/Viewer.cs
+++ EditorHtml/Viewer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text.RegularExpressions;
+
 namespace EditorHtml
 {
     public class Viewer
@@ -8,21 +9,41 @@ namespace EditorHtml
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
-            Console.WriteLine("Modo Visualização");
-            Console.WriteLine("---------");
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("----------------------------------------");
             Replace(text);
-            Console.WriteLine("---------");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
             Console.ReadKey();
             Menu.Show();
         }
 
         public static void Replace(string text)
         {
-            //Regex substitui strigs
             var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
-            Console.WriteLine(strong);
+            var words = text.Split(' ');
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            ((words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>'))
+                        )
+                    );
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
         }
     }
 }
