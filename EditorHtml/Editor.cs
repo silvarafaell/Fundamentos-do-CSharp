63ff3156b1b6637866efd146603b9f2f0018249f
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-02-12 13:18:22 -0300

    Editor HTML - Editor

---
+++ EditorHtml/Editor.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Modo Editor");
+            Console.WriteLine("---------");
+            Start();
+        }
+
+        public static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("---------");
+            Console.WriteLine("Deseja salvar o arquivo?");
+
+        }
+    }
+}
--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -67,7 +67,7 @@ namespace EditorHtml
         {
             switch (option)
             {
-                case 1: Console.WriteLine("Editor"); break;
+                case 1: Editor.Show(); break;
                 case 2: Console.WriteLine("View"); break;
                 case 0:
                     {
