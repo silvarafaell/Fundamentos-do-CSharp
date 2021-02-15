70c77a65fdeb421365abc5cf4cadbfe20c960005
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-15 00:08:33 -0300

    Moedas - Math

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -9,13 +9,11 @@ namespace Numbers
         {
             Console.Clear();
 
-            decimal valor = 10.25m;
+            decimal valor = 10536.25m;
 
-            Console.WriteLine(
-                valor.ToString(
-                    "C",
-                    CultureInfo.CreateSpecificCulture("pt-BR")
-                ));
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
         }
     }
 }
