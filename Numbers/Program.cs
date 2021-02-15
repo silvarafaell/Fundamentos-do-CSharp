c8b13e86d9a383887527bd11c6f9f6c73918087a
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-14 23:49:43 -0300

    Moedas - Tipo para moedas

---
+++ Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.25m;
+
+            Console.WriteLine(valor);
+        }
+    }
+}
