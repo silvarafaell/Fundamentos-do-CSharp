b492082b03466be362ad56d93e244e7ba49ad214
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-15 06:19:56 -0300

    Arrays

---
+++ DotnetLists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotnetLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new Teste[2];
+            meuArray[0] = new Teste();
+
+            Console.WriteLine(meuArray[0].Id);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            Console.WriteLine(meuArray[4]);
+
+        }
+    }
+
+    struct Teste
+    {
+        public int Id { get; set; }
+    }
+}
