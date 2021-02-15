56083eda0a5a59257b464f0a0b3c66b80f60e78e
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-02-15 18:53:10 -0300

    Exceptions - Disparando excessoes

--- DotnetLists/Program.cs
+++ DotnetLists/Program.cs
@@ -10,16 +10,43 @@ namespace DotnetLists
 
             try
             {
-                for (var index = 0; index < 10; index++)
-                {
-                    Console.WriteLine(arr[index]);
-                }
-
+                // for (var index = 0; index < 10; index++)
+                // {
+                //     Console.WriteLine(arr[index]);
+                // }
+                Cadastrar("");
             }
-            catch
+            catch (IndexOutOfRangeException ex)
             {
-                Console.WriteLine("Algo deu errado!");
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Não encontrei o índice na lista");
             }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Falha ao cadastrar texto");
+            }
+            // catch (MinhaException ex)
+            // {
+            //     Console.WriteLine(ex.InnerException);
+            //     Console.WriteLine(ex.Message);
+            //     Console.WriteLine(ex.QuandoAconteceu);
+            //     Console.WriteLine("Exceção customizada");
+            // }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ops, algo deu errado!");
+            }
+        }
+
+        private static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new ArgumentNullException("o texto não pode se nulo ou vazio");
         }
     }
 }
